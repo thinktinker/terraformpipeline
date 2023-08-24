edb62389985d64f19a4fd3ed9c1eecf58bec36cc
Leong Martin <leongmartin@Leongs-MacBook-Air.local>
2023-08-24 17:14:34 +0800

    changed to local key implementation

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ resource "aws_instance" "MartinTfWebServer" {
   ami                         = "ami-0f34c5ae932e6f0e4" # Amazon Linux 2 LTS
   instance_type               = "t2.micro"
   count                       = 2                                                           # number of AWS instances
-  key_name                    = aws_key_pair.martinterraformkeypair.key_name                # for SSH connection with a new keypair created below
+  key_name                    = "martinterraformkeypair-us-east-1-keypair"                  # Use a key created previously on AWS
   subnet_id                   = "subnet-012fda4fc806b7308"                                  # points to the subnet from the used VPC                                       
   vpc_security_group_ids      = [aws_security_group.martin_allow_ssh_icmp_https_traffic.id] # Points to the security group below
   associate_public_ip_address = true
@@ -29,7 +29,7 @@ resource "aws_instance" "MartinTfWebServer" {
 resource "aws_instance" "MartinAnsibleServer" {
   ami                         = "ami-0f34c5ae932e6f0e4" # Amazon Linux 2 LTS
   instance_type               = "t2.micro"
-  key_name                    = aws_key_pair.martinterraformkeypair.key_name                # for SSH connection with a new keypair created below
+  key_name                    = "martinterraformkeypair-us-east-1-keypair"                  # Use a key created previously on AWS
   subnet_id                   = "subnet-012fda4fc806b7308"                                  # points to the subnet from the used VPC                                       
   vpc_security_group_ids      = [aws_security_group.martin_allow_ssh_icmp_https_traffic.id] # Points to the security group below
   associate_public_ip_address = true
@@ -52,22 +52,6 @@ terraform {
     bucket = "martins3terraform" # this bucket has to be created first
     key    = "s3terraformkey"    # this is where the terraform state is written to
     region = "us-east-1"
-  }
-}
-
-# Configure the keypair resource
-# Below resources will create a key pair to your local computer on the same path as your terraform folder
-resource "tls_private_key" "private_key" {
-  algorithm = "RSA"
-  rsa_bits  = 4096
-}
-
-resource "aws_key_pair" "martinterraformkeypair" {
-  key_name   = "martinterraformkeypair-us-east-1-keypair" # this creates a key pair on AWS (you don't have to do it on AWS console)
-  public_key = tls_private_key.private_key.public_key_openssh
-
-  provisioner "local-exec" { # Downloads the keypair: martinterraformkeypair-us-east-1-keypair
-    command = "echo '${tls_private_key.private_key.private_key_pem}' > ./martinterraformkeypair-us-east-1-keypair"
   }
 }
 
